13b79223cc52c47ab3843b35c24724bdd95cf963
Tarun_Kumar <31914542+codes29@users.noreply.github.com>
2019-02-21 01:48:15 +0530

    Delete UserImage.java

--- app/src/main/java/com/codes29/rfidreader/UserImage.java
+++
@@ -1,9 +0,0 @@
-package com.codes29.rfidreader;
-
-public class UserImage {
-    public int imageID;
-    public String tagID;
-    public int examID;
-    public String studentName;
-    public String imageURL;
-}
